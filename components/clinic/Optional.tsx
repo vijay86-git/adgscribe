bc2cda9f46863cf03aa8793e5b934b6872038122
Vijay Singh <vijay.singh@adgonline.in>
2025-07-13 22:19:23 +0530

    changes

--- components/clinic/Optional.tsx
+++ components/clinic/Optional.tsx
@@ -13,38 +13,16 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 
-import { HelpCircle, Check, ChevronsUpDown } from "lucide-react";
+import { Check } from "lucide-react";
 
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 
 import { MultiSelect } from "@/components/multi-select";
-
-type MetaCol = {
-    id: string;
-    name: string;
-};
-
-type CType = {
-    no_of_doctors?: number;
-    daily_monthly_patient_footfall?: number;
-    designation?: string;
-    website_clinic_url?: string;
-    year_establishment?: string;
-    ai_filter?: string;
-    specializations?: string[]
-};
-
-type Spec = {
-    value: string;
-    label: string;
-};
 
 import { ClinicOptionalProps, FormValidationErrors } from "@/components/clinic/Types";
 
@@ -53,19 +31,14 @@ import { updateClinicBusinessDetails } from "@/app/actions"
 import { clinicProfileOptionalSchema, ClinicProfileOptionalSchema } from "@/schemas/clinicProfileOptionalSchema"
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-// type FormData = z.infer<typeof clinicProfileOptionalFormSchema>;
-// type FormErrors = Partial<Record<keyof FormData, string[]>>;
 
 export default function Optional({ designations, specializations, clinic_detail }: ClinicOptionalProps) {
-
 
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
     const [isloading, setIsLoading] = useState<boolean>(false);
     const [serverMessage, setServerMessage] = useState<string>("");
     const [updateMsg, setUpdateMsg] = useState<boolean>(false);
     const [open, setOpen] = useState<boolean>(false);
-    const [countrySearch, setCountrySearch] = useState<string>("");
-
     const [formError, setFormErrors] = useState<FormValidationErrors>({});
 
     const [selectedSpecializations, setSelectedSpecializations] = useState<string[]>(clinic_detail.specializations ?? []);
@@ -87,8 +60,6 @@ export default function Optional({ designations, specializations, clinic_detail 
             ai_filter: clinic_detail?.ai_filter || 0
         },// Connects Zod schema to React Hook Form
     });
-
-    console.log(errors, 999);
 
     // When clinic_detail changes, update form values
     useEffect(() => {
@@ -119,106 +90,7 @@ export default function Optional({ designations, specializations, clinic_detail 
         } else {
             //setServerMessage(true);
         }
-
-        // setIsSubmitting(true);
-        // setUpdateMsg(false);
-        // setServerMessage('');
-        // const response = await updateProfile(data);
-        // setIsSubmitting(false);
-        // if (response.success) {
-        //     setUpdateMsg(true);
-        // } else {
-        //     setServerMessage("Something went wrong! Try again");
-        // }
     }
-
-
-
-    // const { no_of_doctors, daily_monthly_patient_footfall, designation, website_clinic_url, year_establishment, ai_filter } = clinic_detail;
-
-    // const [formData, setFormData] = useState<FormData>({ no_of_doctors, daily_monthly_patient_footfall, designation, website_clinic_url, year_establishment });
-
-    // //const [selectedSpecializations, setSelectedSpecializations] = useState<string[] | (() => string[])>(clinic_detail.specializations ?? []);
-    // const [selectedSpecializations, setSelectedSpecializations] = useState<string[]>(clinic_detail.specializations ?? []);
-
-    // const [errors, setErrors] = useState<FormErrors>({});
-
-    // const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-    // const [serverMessage, setServerMessage] = useState<string>("");
-
-    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     setFormData({
-    //         ...formData,
-    //         [e.target.name]: e.target.value,
-    //     });
-    // };
-
-    // const validateForm = (data: FormData): FormErrors => {
-
-    //     try {
-    //         clinicProfileOptionalFormSchema.parse(data);
-    //         return {};
-    //     } catch (error) {
-    //         if (error instanceof z.ZodError) {
-    //             return error.flatten().fieldErrors;
-    //         }
-    //         return {};
-    //     }
-    // };
-
-    //async function handleSubmit(e: React.FormEvent) {
-    /*
-      e.preventDefault();
- 
-      const newErrors = validateForm(formData);
-      setErrors(newErrors);
- 
-      console.log(Object.keys(newErrors).length, newErrors);
- 
-      if (Object.keys(newErrors).length === 0) {
- 
-        //setFormErrors({});
- 
-        try {
-            setServerMessage('');
-            setIsSubmitting(true);
- 
-            const res = await fetch(`/api/clinic/optional`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                                    ...formData,
-                                    specializations: selectedSpecializations,
-                                  })
-            });
- 
-            const data = await res.json();
-            setIsSubmitting(false);
- 
-            if (data.success) {
-                //setUpdateMsg(true);
-            } 
- 
-            if (data?.msg?.errors) {
-               // setFormErrors(data.msg.errors);
-            }
- 
-            if (data?.msg?.message) {
-                setServerMessage(data.msg.message);
-            }
- 
-        } catch (err: unknown) {
-          setIsSubmitting(false);
-          if (err instanceof Error) {
-            setServerMessage(err.message); // // works, `e` narrowed to string
-          } else if (e instanceof Error) {
-            setServerMessage("Oops! Something went wrong"); // works, `e` narrowed to Error
-          }
-        }
-      }*/
-    //}
 
     return (
         <Card>
